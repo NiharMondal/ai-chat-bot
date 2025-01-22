23737ca86d122e0e4852b4f9efe2030e2ce9afd2
Nihar Mondal <developernihar@gmail.com>
2025-01-22 21:02:44 +0600

    style: changed header position for small and desktop version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import ThemeSwitch from "@/components/themes/SwitchTheme";
 export default function Home() {
 	return (
 		<main className="overflow-hidden">
-			<h1 className="text-center text-3xl font-semibold font-mono py-5">
+			<h1 className="pl-5 md:pl-0 md:text-center text-3xl font-semibold font-mono py-5">
 				Chat With AI 🤖
 			</h1>
 			<MessageForm />
