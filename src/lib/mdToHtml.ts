0872440d51282786e56d46d15a2a7d904b2b7e3c
Nihar Mondal <developernihar@gmail.com>
2025-01-22 19:31:27 +0600

    remove: comment from renderMarkdownToHTML

--- src/lib/mdToHtml.ts
+++ src/lib/mdToHtml.ts
@@ -3,9 +3,6 @@ import { Remarkable } from "remarkable";
 const md = new Remarkable();
 
 export function renderMarkdownToHTML(markdown: string) {
-	// This is ONLY safe because the output HTML
-	// is shown to the same user, and because you
-	// trust this Markdown parser to not have bugs.
 	const renderedHTML = md.render(markdown);
 	return { __html: renderedHTML };
 }
